23ac4a08f60d4397e35ca1a602a7f5a175fa24eb
alwright33 <austinlucaswright@gmail.com>
2024-08-05 11:08:45 -0500

    built firePottery function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,23 @@
-// Imports go first
-
-import { makePottery } from "./PotteryWheel.js"
-
-
+import { makePottery } from "./PotteryWheel.js";
+import { firePottery } from "./kiln.js";
 
 // Make 5 pieces of pottery at the wheel
+const mug = makePottery("Mug", 5.3, 6);
+const vase = makePottery("Vase", 7.0, 8);
+const bowl = makePottery("Bowl", 4.2, 5);
+const plate = makePottery("Plate", 9.1, 1.5);
+const cup = makePottery("Cup", 3.0, 4);
 
-const mug = makePottery('Mug', 5.3, 6)
-const vase = makePottery('Vase', 7.4, 10)
-const bowl = makePottery('Bowl', 4.2, 3)
-const cup = makePottery('Cup', 3, 6,)
-const plate = makePottery('Plate', 3.3, 2)
-
-console.log(potteryItem);
 // Fire each piece of pottery in the kiln
+const firedMug = firePottery(mug, 2100);
+const firedVase = firePottery(vase, 2300);
+const firedBowl = firePottery(bowl, 2200);
+const firedPlate = firePottery(plate, 2000);
+const firedCup = firePottery(cup, 2250);
 
-
-// Determine which ones should be sold, and their price
-
-
-// Invoke the component function that renders the HTML list
-
+// View the fired pottery objects
+console.log(firedMug);
+console.log(firedVase);
+console.log(firedBowl);
+console.log(firedPlate);
+console.log(firedCup);
